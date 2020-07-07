3a2ae7f0d646c8b330a65d4e6f33ee75e7d828fe
Manju Jonchhe <>
2020-07-06 21:26:59 -0400

    recommit all

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,53 @@
-var app = angular.module('FinanceTrackerApp',[]);
+var app = angular.module('FinanceTrackerApp',[])
+                .factory('stockService', ['$http', function($http){
+                    var stockApi = {};
+                    stockApi.searchStocks = function(symbol){
+                      return $http.get('/search_stocks.json?stock=' + symbol);
+                    }
+                    stockApi.addStockToPortfolio = function(symbol) {
+                      return $http.post('/user_stocks.json?stock=' + symbol);
+                    }
+
+                    return stockApi;
+                  }])
+                .controller('stocksController', ['$scope', 'stockService', function($scope, stockService){
+                  $scope.stock = {};
+                  $scope.lookup = function(){
+                    if($scope.ticker != undefined && $scope.ticker != '') {
+                      stockService.searchStocks($scope.ticker)
+                        .then(function(response){
+                          $scope.stock = {};
+                          $scope.stock.error = null;
+                          $scope.stock.message = null;
+                          $scope.stock.symbol = response.data.ticker;
+                          $scope.stock.name = response.data.name;
+                          $scope.stock.last_price = response.data.last_price;
+                          $scope.stock.can_be_added = response.data.can_be_added;
+                        }, function(response){
+                          $scope.stock = {};
+                          $scope.stock.error = response.data.response;
+                        });
+                    } else {
+                      $scope.stock = {}
+                    }
+                  }
+
+                  $scope.add = function(){
+                    if($scope.stock != undefined && $scope.stock.symbol != ''){
+                      stockService.addStockToPortfolio($scope.stock.symbol)
+                        .then(function(response){
+                          $scope.stock.error = null;
+                          $scope.stock.message = response.data.response;
+                          $scope.stock.name     = null;
+                          $scope.ticker         = null;
+
+                          $('#stock-list').load('my_portfolio.js');
+                        },function(response){
+                          $scope.stock = {};
+                          $scope.stock.error = response.data.response;
+                        });
+                    } else {
+                      $scope.stock.error = "Stock cannot be added";
+                    }
+                  }
+                }]);
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,18 @@
 class UsersController < ApplicationController
-  
+
   def my_portfolio
     @user_stocks = current_user.stocks
     @user = current_user
+    respond_to do |format|
+      format.html { render :my_portfolio }
+      format.js   { render partial: 'stocks/list.html' }
+    end
   end
-  
+
   def my_friends
     @friendships = current_user.friends
   end
-  
+
   def search
     if params[:search_param].blank?
       flash.now[:danger] = "You have entered an empty search string"
@@ -21,7 +25,7 @@ class UsersController < ApplicationController
       format.js { render partial: 'friends/result' }
     end
   end
-  
+
   def add_friend
     @friend = User.find(params[:friend])
     current_user.friendships.build(friend_id: @friend.id)
@@ -29,13 +33,13 @@ class UsersController < ApplicationController
       flash[:notice] = "Friend was successfully added"
     else
       flash[:danger] = "There was something wrong with the friend request"
-    end  
+    end
     redirect_to my_friends_path
   end
-  
+
   def show
     @user = User.find(params[:id])
     @user_stocks = @user.stocks
   end
-  
+
 end
